92951e4c5833de71220a7b6651f1a16420597886
Dakota <54693749+UnambiguousUsername@users.noreply.github.com>
2022-12-20 18:25:25 -0500

    Update
    Testing with smaller input.
    Set up how I think the code should be structured.

--- 2022/day-9/part-1.py
+++ 2022/day-9/part-1.py
@@ -2,9 +2,18 @@ raw_file_contents = ''
 with open('input.txt', 'rt') as input:
 	raw_file_contents = input.readlines()
 
-height_radius = 0
-width_radius = 0
-current_hr = 0
-current_wr = 0
+tail_set = set([0,0])
+h_coords = [0,0]
+t_coords = [0,0]
 for line in raw_file_contents:
 	direction, steps = line.rstrip().split(' ')
+
+	match direction:
+		case 'U':
+			
+		case 'R':
+			pass
+		case 'D':
+			pass
+		case 'L':
+			pass
